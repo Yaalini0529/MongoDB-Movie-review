8f510c8e6affea1637f3e8cf16754acaa329e371
Yaalini0529 <yaalini2907@gmail.com>
2026-05-26 15:25:22 +0530

    Update mongodb_queries.js

--- mongodb_queries.js
+++ mongodb_queries.js
@@ -1,5 +1,26 @@
 use movie_reviews
 
+db.movies.insertMany([
+{
+  title: "Inception",
+  genre: "Sci-Fi",
+  rating: 9,
+  year: 2010
+},
+{
+  title: "Interstellar",
+  genre: "Sci-Fi",
+  rating: 10,
+  year: 2014
+},
+{
+  title: "Joker",
+  genre: "Drama",
+  rating: 8,
+  year: 2019
+}
+])
+
 db.movies.find()
 
 db.movies.find({genre: "Sci-Fi"})
